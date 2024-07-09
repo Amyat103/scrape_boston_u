bd46eb1514a7ffc8266a86338fb5d5b1464e4cc8
Amyat103 <zhoudavid7312@gmail.com>
2024-07-09 16:02:25 -0400

    setup chrome driver

--- src/login.py
+++ src/login.py
@@ -1,4 +1,21 @@
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import keys
+# import time
+
+# driver = 
+
 from selenium import webdriver
-from selenium.webdriver.common.keys import keys
-import time
+import os
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+
+options.add_argument("--window-size=1920,1080")
+
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=options)
+driver.get("https://www.youtube.com/")
+
+
 
